566faed55b08f39094708c2facc7f5834c392d5f
Zentryx <zentryx.official@gmail.com>
2025-10-21 23:13:04 +0700

    Update App Routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,12 @@
-import HomePage from "./pages/home/page"
+// Route
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+// Layout
 import Layout from "./pages/layout";
+
+// Pages
+import HomePage from "./pages/home/page";
+import SignupPage from "./pages/signup/page";
 
 
 function App() {
@@ -15,7 +21,7 @@ function App() {
           </Route>
 
           <Route path="/signup" element={<SignupPage />} />
-          
+
         </Routes>
       </Router>
     </>
