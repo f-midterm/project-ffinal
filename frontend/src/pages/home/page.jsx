43143911ccd9baa0d518617a41950de088f25637
Tangg123 <pititang12345@gmail.com>
2025-10-22 01:58:38 +0700

    home navbar is coming

--- frontend/src/pages/home/page.jsx
+++ frontend/src/pages/home/page.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import Hero from '../../components/hero/hero'
-
-
+import HomeNavbar from '../../components/navbar/home-navbar'   // 
 function HomePage() {
   return (
     <div>
-      <Hero />
-      <div id='explore' className='py-24'>
-        <div className='max-w-4xl mx-auto text-center'>
-          <div>
-            
+      <HomeNavbar />
+
+      <div className="pt-24">
+        <Hero />
+
+        <div id='explore' className='py-24'>
+          <div className='max-w-4xl mx-auto text-center'>
+            <div>
+            </div>
           </div>
         </div>
       </div>
