4690b473502b2ed1b3e3295d5cdad002911786ff
Zentryx <zentryx.official@gmail.com>
2025-10-22 11:16:49 +0700

    Rework HomePage

--- frontend/src/pages/home/page.jsx
+++ frontend/src/pages/home/page.jsx
@@ -1,18 +1,15 @@
 import React from 'react'
 import Hero from '../../components/hero/hero'
-import HomeNavbar from '../../components/navbar/home-navbar'   // 
+
 function HomePage() {
   return (
     <div>
-      <HomeNavbar />
+      <Hero />
 
-      <div className="pt-24">
-        <Hero />
-
-        <div id='explore' className='py-24'>
-          <div className='max-w-4xl mx-auto text-center'>
-            <div>
-            </div>
+      <div id='explore' className='py-24'>
+        <div className='max-w-4xl mx-auto text-center'>
+          <div>
+            
           </div>
         </div>
       </div>
